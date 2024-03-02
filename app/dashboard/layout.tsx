61212dbaf9e82255054b73a09f520765ed94a558
tomitheninja <sudi.tamas@simonyi.bme.hu>
2024-03-02 23:46:58 +0100

    Add dynamic flag to dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 
+export const dynamic = 'force-dynamic';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">

3a7fb2cc57107fa5c546271e53a22ab71bc33649
tomitheninja <sudi.tamas@simonyi.bme.hu>
2024-03-02 21:51:16 +0100

    Implement AuthGuard on /dashboard

--- app/ui/dashboard/signout-button.tsx
+++ app/ui/dashboard/signout-button.tsx
@@ -13,7 +13,9 @@ export default function SignOutForm() {
     <form>
       <h1 className="text-xl md:text-2xl">Üdv {session?.user?.email}!</h1>
       <button
-        onClick={() => signOut({ callbackUrl: '/' })}
+        onClick={() => {
+          signOut({ callbackUrl: '/' });
+        }}
         className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
       >
         <div className="hidden md:block">Kijelentkezés</div>

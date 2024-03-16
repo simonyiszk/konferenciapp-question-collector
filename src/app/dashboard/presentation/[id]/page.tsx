e6106b079c4799bba099a2cad1e0c77d99afb557
tomitheninja <sudi.tamas@simonyi.bme.hu>
2024-03-16 03:02:37 +0100

    Add presentation provider

--- src/app/dashboard/presentation/[id]/page.tsx
+++ src/app/dashboard/presentation/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { QuestionState } from '@prisma/client';
 import { FiInbox, FiStar } from 'react-icons/fi';
 
+import { PresentationProvider } from '@/components/providers';
 import { isPresentationCurrent } from '@/lib/presentation.utils';
 import { prisma } from '@/server-lib/prisma';
 import QuestionGrid from '@/ui/dashboard/presentation/question-grid';
@@ -28,33 +29,35 @@ export default async function Page({ params }: { params: { id: string } }) {
   if (!presentation) throw new Error('Invalid presentation id');
 
   return (
-    <main className="space-y-4 p-10">
-      <h1>{presentation.title}</h1>
-      <div className="flex space-x-2 text-sm text-white">
-        {presentation.room && (
-          <span className="rounded-full bg-red-500 p-1.5 px-4">
-            {presentation.room}
-          </span>
-        )}
-        {isPresentationCurrent(presentation) && (
-          <span className="rounded-full bg-green-500 p-1.5 px-4">Live</span>
-        )}
-      </div>
-      <div className="flex w-full flex-col gap-5 md:flex-row">
-        <TimeCard start={presentation.start} end={presentation.end} />
-        <StatsCard title="Beérkezett" icon={FiInbox}>
-          {presentation.questions.length}
-        </StatsCard>
-        <StatsCard title="Megjelölt" icon={FiStar}>
-          {
-            presentation.questions.filter(
-              (q) => q.mark === QuestionState.SELECTED,
-            ).length
-          }
-        </StatsCard>
-      </div>
-      <span className="mb-4 mt-4 block h-1 w-32 rounded-lg bg-gray-300" />
-      <QuestionGrid questions={presentation.questions} />
-    </main>
+    <PresentationProvider value={presentation}>
+      <main className="space-y-4 p-10">
+        <h1>{presentation.title}</h1>
+        <div className="flex space-x-2 text-sm text-white">
+          {presentation.room && (
+            <span className="rounded-full bg-red-500 p-1.5 px-4">
+              {presentation.room}
+            </span>
+          )}
+          {isPresentationCurrent(presentation) && (
+            <span className="rounded-full bg-green-500 p-1.5 px-4">Live</span>
+          )}
+        </div>
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          <TimeCard start={presentation.start} end={presentation.end} />
+          <StatsCard title="Beérkezett" icon={FiInbox}>
+            {presentation.questions.length}
+          </StatsCard>
+          <StatsCard title="Megjelölt" icon={FiStar}>
+            {
+              presentation.questions.filter(
+                (q) => q.mark === QuestionState.SELECTED,
+              ).length
+            }
+          </StatsCard>
+        </div>
+        <span className="mb-4 mt-4 block h-1 w-32 rounded-lg bg-gray-300" />
+        <QuestionGrid questions={presentation.questions} />
+      </main>
+    </PresentationProvider>
   );
 }

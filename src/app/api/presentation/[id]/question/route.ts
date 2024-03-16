95300317497a435abc520dd50350d053b7fcd25d
tomitheninja <sudi.tamas@simonyi.bme.hu>
2024-03-16 03:01:51 +0100

    Refactor question api

--- src/app/api/presentation/[id]/question/route.ts
+++ src/app/api/presentation/[id]/question/route.ts
@@ -1,15 +1,11 @@
-import { validate } from 'class-validator';
-
 import { prisma } from '@/server-lib/prisma';
 import {
   BadRequestResponse,
   InternalServerErrorResponse,
   OkResponse,
 } from '@/server-lib/responses';
-import {
-  CreateQuestionDto,
-  CreateQuestionInput,
-} from '@/types/CreateQuestionsDto';
+
+import { PresentationQuestionService } from './service';
 
 export async function GET(
   req: Request,
@@ -31,36 +27,13 @@ export async function GET(
   }
 }
 
+const service = new PresentationQuestionService();
+
 export async function POST(
   req: Request,
   { params }: { params: { id: string } },
 ) {
-  const data = CreateQuestionInput.fromPlain({
-    ...((await req.json()) as CreateQuestionDto),
-    presentationId: params.id,
-  });
-
-  const errors = await validate(data, { forbidUnknownValues: true });
-  if (errors.length !== 0) {
-    return BadRequestResponse(JSON.stringify(errors));
-  }
-
-  try {
-    const question = await prisma.question.create({
-      data: {
-        content: data.content,
-        presentation: { connect: { id: data.presentationId } },
-        user: {
-          connectOrCreate: {
-            where: { id: data.userId },
-            create: { id: data.userId },
-          },
-        },
-      },
-    });
-    return OkResponse(question);
-  } catch (e) {
-    console.log(e);
-    return InternalServerErrorResponse(JSON.stringify(e));
-  }
+  const { data, errors } = await service.validate(params.id, await req.json());
+  if (errors || !data) return BadRequestResponse(JSON.stringify(errors));
+  return service.create(data);
 }
---
+++ src/app/api/presentation/[id]/question/service.ts
@@ -0,0 +1,62 @@
+import { QuestionState } from '@prisma/client';
+import { validate } from 'class-validator';
+
+import { prisma } from '@/server-lib/prisma';
+import {
+  InternalServerErrorResponse,
+  OkResponse,
+} from '@/server-lib/responses';
+import {
+  CreateQuestionDto,
+  CreateQuestionInput,
+} from '@/types/CreateQuestionsDto';
+
+export class PresentationQuestionService {
+  async create({
+    content,
+    userId,
+    presentationId,
+    selected = false,
+  }: CreateQuestionInput & { selected?: boolean }) {
+    try {
+      const question = await prisma.question.create({
+        data: {
+          content,
+          mark: selected ? QuestionState.SELECTED : undefined,
+          presentation: { connect: { id: presentationId } },
+          user: {
+            connectOrCreate: {
+              where: { id: userId },
+              create: { id: userId },
+            },
+          },
+        },
+      });
+      return OkResponse(question);
+    } catch (e) {
+      console.log(e);
+      return InternalServerErrorResponse(JSON.stringify(e));
+    }
+  }
+  async validate(presentationId: string, body: CreateQuestionDto) {
+    const data = CreateQuestionInput.fromPlain({
+      ...body,
+      presentationId,
+    });
+
+    const errors = await validate(data, { forbidUnknownValues: true });
+    return errors.length === 0 ? { data } : { errors };
+  }
+
+  async findByUser(presentationId: string, userId: string) {
+    try {
+      const questions = await prisma.question.findMany({
+        where: { presentationId, userId },
+      });
+      return OkResponse(questions);
+    } catch (e) {
+      console.log(e);
+      return InternalServerErrorResponse(JSON.stringify(e));
+    }
+  }
+}
